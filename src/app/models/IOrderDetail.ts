f1922a0aef8f1312957ab31fa8f7f87dc738cbc2
J-Graham <johngrhm@gmail.com>
2020-07-24 14:58:03 -0400

    Updates for clarity

--- src/app/models/IOrderDetail.ts
+++ src/app/models/IOrderDetail.ts
@@ -1,4 +1,4 @@
-export interface IOrder {
+export interface IOrderDetail {
     id: number;
     name: string;
     price: number;
--- src/app/models/IOrderSummary.ts
+++ src/app/models/IOrderSummary.ts
@@ -1,9 +1,9 @@
-import { IOrder } from './IOrder';
+import { IOrderDetail } from './IOrderDetail';
 import { IShipping } from './IShipping';
 import { ITax } from './ITax';
 
 export interface IOrderSummary {
-    orders: IOrder[];
+    orders: IOrderDetail[];
     shipping: IShipping;
     tax?: ITax;
     orderTotal?: number;
